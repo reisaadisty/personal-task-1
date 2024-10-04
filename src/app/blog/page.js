ef57fb5d6bfccc55cbc5894ff870d653c5ed8644
reisaadisty <reisaadistyy@gmail.com>
2024-10-04 14:48:41 +0700

    feat: menambahkan fitur CRUD

---
+++ src/app/blog/page.js
@@ -0,0 +1,10 @@
+import { Navbar } from "@/components/navbar";
+
+export default function BlogPage(){
+    return (
+        <div>
+            <Navbar/>
+            <h1>Blog Page</h1>
+        </div>
+    );
+}
